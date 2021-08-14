0ee35f1a6059ad7f3a0ae30d7680bb237881bfc1
vandniBirla <78420545+vandniBirla@users.noreply.github.com>
2021-08-14 21:55:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var bg, bgImg;
+var rein, reinImg;
+var snow;
+function preload(){
+  reinImg=loadAnimation("rein1.png","rein2.png")
+  bgImg=loadImage("snow2.jpg")
+}
+function setup() {
+  createCanvas(800,400);
+ bg= createSprite(200, 10, 50, 50);
+ bg.addImage(bgImg)
+ bg.velocityX=6;
+ rein=createSprite(400,400-80,20,20);
+ rein.addAnimation("rein",reinImg);
+}
+
+function draw() {
+  background(255,255,255);
+  if(bg.x>400){
+    bg.x=200
+  }
+  spawn();
+  drawSprites();
+}
+function spawn(){
+  if(frameCount%1===0){
+    snow=new Snow(random(20,780),10,20,20)
+  }
+}
